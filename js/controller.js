fd330c44bcc4ce6c9f90520f7d27b8bf940d6572
sabrinakmdantas <sabrinakmdantas@gmail.com>
2026-08-30 16:33:57 -0300

    Update header comment in controller.js
    Added author and modification details in the header comment.

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,13 @@
 /**
  * @author Filipe Caixeta / http://filipecaixeta.com.br/
  
-
+/**
+ * @author Filipe Caixeta / http://filipecaixeta.com.br/
+ *
+ * Arquivo modificado para o PlatD-CNC (2024-2026)
+ * IFRN - Instituto Federal de Educacao, Ciencia e Tecnologia do RN
+ * Ver NOTICE.md para o detalhamento das modificacoes.
+ 
 CWS.Controller = function (editor,storage,renderer,motion,autoRun) 
 	{
 		this.storage = storage;
