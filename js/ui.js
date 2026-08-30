d9b2c1b2f36de3b9da7378d296c8304c348b04b7
sabrinakmdantas <sabrinakmdantas@gmail.com>
2026-08-30 16:34:27 -0300

    Revise header comments in ui.js
    Updated file header comments to reflect modifications for PlatD-CNC.

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,15 @@
 /**
  * @author Filipe Caixeta / http://filipecaixeta.com.br/
  */
-/** 
+/**
+
+/**
+ * @author Filipe Caixeta / http://filipecaixeta.com.br/
+ *
+ * Arquivo modificado para o PlatD-CNC (2024-2026)
+ * IFRN - Instituto Federal de Educacao, Ciencia e Tecnologia do RN
+ * Ver NOTICE.md para o detalhamento das modificacoes.
+ 
 
 CWS.UI = function (controller) 
 	{
