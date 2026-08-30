fcb52f2fb0b0bd527e92fc45f4e32dfa82e0223f
sabrinakmdantas <sabrinakmdantas@gmail.com>
2026-08-30 16:38:31 -0300

    Revise comments for circular interpolation in interpreter.js
    Updated author comments and fixed circular interpolation documentation.

--- js/interpreter.js
+++ js/interpreter.js
@@ -1,13 +1,14 @@
 /**
  * @author Filipe Caixeta / http://filipecaixeta.com.br/
- * Fixed circular interpolation (G02/G03) by Claude
- */
-/**
- * @author Filipe Caixeta / http://filipecaixeta.com.br/
  *
  * Arquivo modificado para o PlatD-CNC (2024-2026)
  * IFRN - Instituto Federal de Educacao, Ciencia e Tecnologia do RN
+ *
+ * Correcao da interpolacao circular G02/G03 implementada pela
+ * equipe do PlatD-CNC.
+ *
  * Ver NOTICE.md para o detalhamento das modificacoes.
+ */
  
 CWS.Interpreter = function (machine) 
 	{
