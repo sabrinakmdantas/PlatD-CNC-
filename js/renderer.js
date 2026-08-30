683e6898a96dc731e7ed4566e565e067a532c2d9
sabrinakmdantas <sabrinakmdantas@gmail.com>
2026-08-30 16:34:46 -0300

    Update author and modification notes in renderer.js
    Updated author information and added modification details.

--- js/renderer.js
+++ js/renderer.js
@@ -3,6 +3,13 @@
  */
 /** 
 
+/**
+ * @author Filipe Caixeta / http://filipecaixeta.com.br/
+ *
+ * Arquivo modificado para o PlatD-CNC (2024-2026)
+ * IFRN - Instituto Federal de Educacao, Ciencia e Tecnologia do RN
+ * Ver NOTICE.md para o detalhamento das modificacoes.
+ 
 CWS.Renderer = function (id,options) 
 	{
 		options = options || {};
